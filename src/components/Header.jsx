bc32d983e9bee4ff970603585c0bf824ef5ea16b
RianWilliamAlday <23-19976@g.batstate-u.edu.ph>
2025-12-17 08:23:08 +0800

    Paragraph text size fix on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header =  ({webname}) =>{
         <>
         <header className="bg-blue-500 text-center p-4 font-serif text-shadow-lg shadow-sm shadow-black-700">
                 <h1 className="m-2 text-3xl xl:text-4xl font-bold text-white">{webname}</h1>
-                <p className="text-[20px]">List mods that you want to download for your apps!</p>
+                <p className="text-[15px] xl:text-[20px]">List mods that you want to download for your apps!</p>
         </header>
         </>
     );
