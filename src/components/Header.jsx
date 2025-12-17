af56d0b30e243d143c1a1e7c350d75b3df4bbccd
RianWilliamAlday <23-19976@g.batstate-u.edu.ph>
2025-12-17 08:19:52 +0800

    Fix text size on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header =  ({webname}) =>{
     return(
         <>
         <header className="bg-blue-500 text-center p-4 font-serif text-shadow-lg shadow-sm shadow-black-700">
-                <h1 className="m-2 text-2xl xl:text-4xl font-bold text-white">{webname}</h1>
+                <h1 className="m-2 text-3xl xl:text-4xl font-bold text-white">{webname}</h1>
                 <p className="text-[20px]">List mods that you want to download for your apps!</p>
         </header>
         </>
